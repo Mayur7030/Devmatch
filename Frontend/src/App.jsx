ce1ef97e73078ea6337bfa693ee83597eb74bc16
Mayur7030 <pawarmr9999@gmail.com>
2026-01-16 21:02:45 +0530

    Navbar,footer done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import "./App.css"
+import React from "react";
+import "./App.css";
+import Home from "./Home";
+import Body from "./Body";
+import Login from "./Login";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (<>
-    <h1 class="bg-base-00" >
-    Hello world!
-  </h1>
-  <input type="checkbox" class="checkbox checkbox-secondary" />
-  </>
-
-  )
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/home" element={<Home />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
