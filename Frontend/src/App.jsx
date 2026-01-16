f4d0f5c51138dab572cde89ed8790e2946fefcab
Mayur7030 <pawarmr9999@gmail.com>
2026-01-16 09:55:59 +0530

    tailwind & daizyui setup done!

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css"
+
+
+function App() {
+  return (<>
+    <h1 class="bg-base-00" >
+    Hello world!
+  </h1>
+  <input type="checkbox" class="checkbox checkbox-secondary" />
+  </>
+
+  )
+}
+
+export default App
